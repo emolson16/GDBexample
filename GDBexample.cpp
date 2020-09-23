98eb21740747ab215d32c4814c5555d6fd9ede72
Emily <emolson@umich.edu>
2020-09-22 23:07:26 -0400

    fix

--- GDBexample.cpp
+++ GDBexample.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int findMax(int arr[], int n){
 
-  int max = arr[0]; // set max to first element
+  int max = 0; // set max to first element
 
   for(int i = 0; i < n; ++i){ // check each element to see if it's bigger than max
     if(arr[i] > max){ 
@@ -21,7 +21,7 @@ int main(){
     int arr[5] = {-4, -2, -5, -6, -7};
     int maxNum = findMax(arr, 5);
 
-    if(maxNum++ == -1){
+    if(maxNum == -2){
       cout << "true" << endl;
     }
 
